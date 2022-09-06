c6a448b3964563c75189f2d63bb53ce985158bc0
mateo <mateo@localhost.localdomain>
2022-09-06 13:18:24 -0400

    Calculator

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import *
+    argv_len = len(sys.argv) - 1
+    if argv_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] == "+":
+        print("{}".format(add(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == "-":
+        print("{}".format(sub(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == "*":
+        print("{}".format(mul(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == "/":
+        print("{}".format(div(int(sys.argv[1]), int(sys.argv[3]))))
+    else:
+        print("Unknown operator. Available operators: +, -, *, and /")
+        sys.exit(1)
