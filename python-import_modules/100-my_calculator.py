d733419b4ef1d60bde28eed46a2dac9ce3695776
mateo <mateo@localhost.localdomain>
2022-09-06 14:00:43 -0400

    Codestyle Fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,20 +7,20 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] == "+":
-        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + '='
-        print(result, end=" ")
+        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' '
+        print("{}=".format(result), end=" ")
         print("{}".format(add(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == "-":
-        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + '='
-        print(result, end=" ")
+        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' '
+        print("{}=".format(result), end=" ")
         print("{}".format(sub(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == "*":
-        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + '='
-        print(result, end=" ")
+        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' '
+        print("{}=".format(result), end=" ")
         print("{}".format(mul(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == "/":
-        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + '='
-        print(result, end=" ")
+        result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' '
+        print("{}=".format(result), end=" ")
         print("{}".format(div(int(sys.argv[1]), int(sys.argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
