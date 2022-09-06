d84f1b86ea72181d6db2cf0c2dbf2956940c9cb6
mateo <mateo@localhost.localdomain>
2022-09-06 08:58:10 -0400

    Task 3 Fixed 1 Arg Case

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     argv_len = len(sys.argv) - 1
     if argv_len == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1:]))
+        print("1: {}".format(sys.argv[1]))
     elif argv_len == 0:
         print("0 arguments.")
     else:
