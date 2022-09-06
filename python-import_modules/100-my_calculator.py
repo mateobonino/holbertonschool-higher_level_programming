eb7f77c40d600bdaf098b49a980406a0d47e7589
mateo <mateo@localhost.localdomain>
2022-09-06 13:54:52 -0400

    Calculator FIx

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,16 +3,21 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import *
     argv_len = len(sys.argv) - 1
+    result = sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + '='
     if argv_len != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] == "+":
+        print(result, end=" ")
         print("{}".format(add(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == "-":
+        print(result, end=" ")
         print("{}".format(sub(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == "*":
+        print(result, end=" ")
         print("{}".format(mul(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == "/":
+        print(result, end=" ")
         print("{}".format(div(int(sys.argv[1]), int(sys.argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, *, and /")
