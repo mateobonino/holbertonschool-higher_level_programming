8a720c833ced83905486721e9a56a91113c9af11
mateo <mateo@localhost.localdomain>
2022-09-12 08:50:24 -0400

    Task 9 fixed negative numbers

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,11 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list or len(my_list) == 0:
         return None
-    max_int = 0
+    max_int = my_list[0]
     for i in range(0, len(my_list), 1):
         if my_list[i] > max_int:
-            if my_list[i] >= 0:
-                max_int = my_list[i]
-            else:
-                max_int = (my_list[i] * -1)
+            max_int = my_list[i]
     return max_int
