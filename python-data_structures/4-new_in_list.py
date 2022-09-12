b688f62611b1b3096097376d28a15bc83712edae
Mateo Bonino <mateo@localhost.localdomain>
2022-09-12 00:43:07 -0300

    Task 4 fixed range issue

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return my_list
     elif idx < 0:
         ret_list = list(my_list)
