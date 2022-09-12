406d28aefe272db28778d040d931e8c5f1da00d6
Mateo Bonino <mateo@localhost.localdomain>
2022-09-12 00:35:04 -0300

    Task 3 fixed "list = none" case

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list) - 1
     if not my_list:
         return
     else:
+        i = len(my_list) - 1
         while i >= 0:
             print("{:d}".format(my_list[i]), end='\n')
             i -= 1
