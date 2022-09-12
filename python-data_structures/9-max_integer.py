5a60796788d6a690fec97e5dc095e765fa39a362
mateo <mateo@localhost.localdomain>
2022-09-12 08:25:26 -0400

    Task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list or len(my_list) == 0:
+        return None
+    max_int = 0
+    for i in range(0, len(my_list), 1):
+        if my_list[i] > max_int:
+            max_int = my_list[i]
+    return max_int
