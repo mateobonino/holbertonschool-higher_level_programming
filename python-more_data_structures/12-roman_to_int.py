019a68fd3f2e80496162193c7b7ffbea9726996b
mateo <mateo@localhost.localdomain>
2022-09-13 15:42:16 -0300

    Task 12 Fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
-        return None
+        return 0
     rn = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     res = []
 
