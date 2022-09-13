52a3a49fc45369c8d760221147534eccdf5ecaec
Mateo Bonino <mateo@localhost.localdomain>
2022-09-12 23:20:58 -0300

    Task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    mul = 0
     ret_dic = a_dictionary.copy()
-    for k in a_dictionary.keys():
-        mul = a_dictionary[k] * 2
-        ret_dic.update({a_dictionary[k]: mul})
+    return ret_dic.update({n: 2 * ret_dic[n] for n in ret_dic.keys()})
