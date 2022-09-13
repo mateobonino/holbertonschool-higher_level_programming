9e3796b64c7002a0dccf00d7f20dcfbe28a0f407
mateo <mateo@localhost.localdomain>
2022-09-13 08:14:28 -0300

    Task 9 fix

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    ret_dic = a_dictionary.copy()
-    return ret_dic.update((x, y * 2) for x, y in ret_dic.items())
+    dc_copy = a_dictionary.copy()
+    mul = 0
+    for k in a_dictionary:
+        mul = a_dictionary[k] * 2
+        dc_copy = dc_copy.replace({a_dictionary[k], mul})
+    return dc_copy
