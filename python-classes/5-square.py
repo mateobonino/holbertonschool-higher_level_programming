8a108c2869e627f4e4e959ca7b350e9031db406d
Mateo Bonino <mateo@localhost.localdomain>
2022-09-20 18:52:50 -0300

    Task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        size (int): The size of the new square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__sie == 0:
+            print("")
+            return
+        for i in range(0, self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+        print("")
