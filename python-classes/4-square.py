8da4c978610a94548a10199649ef73fe0ab7e978
Mateo Bonino <mateo@localhost.localdomain>
2022-09-20 18:31:28 -0300

    Task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square():
         """Sets the size to a value.""""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
