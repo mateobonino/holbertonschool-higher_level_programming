bd74a4358e537b5687ec4bff23b77ebb3d600021
Mateo Bonino <mateo@localhost.localdomain>
2022-09-20 18:33:42 -0300

    Task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,7 +2,7 @@
 """Define a class Square."""
 
 
-class Square():
+class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
@@ -10,14 +10,14 @@ class Square():
         size (int): The size of the new square"""
         self.__size = size
 
-    @propety
+    @property
     def size(self):
         """Get/set the current size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size to a value.""""
+        """Sets the size to a value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
