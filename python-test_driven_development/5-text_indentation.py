5e672772333bd847cee52f4739b5655a3781fb83
mateo <mateo@localhost.localdomain>
2022-09-22 18:47:05 -0300

    Task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Prints a text"""
+
+
+def text_indentation(text):
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:" or text[i] == "\n":
+            print("\n")
+            i += 1
+            while i < len(text) and (text[i] == ' ' or text[i] == '\t'):
+                i += 1
+            continue
+        i += 1
+    print()
