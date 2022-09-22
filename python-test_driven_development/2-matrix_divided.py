201f09b30f8fd6c59f333a862c8d3b66ddb961c3
mateo <mateo@localhost.localdomain>
2022-09-22 16:10:38 -0300

    Task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,6 +15,11 @@ def matrix_divided(matrix, div):
     for i in matrix:
         if len(i) != matrix_len:
             raise TypeError("Each row of the matrix must have the same size")
+    for j in matrix:
+        for k in j:
+            if type(k) is not int and type(k) is not float:
+                raise TypeError("matrix must be a matrix (list of lists)" +  
+                    "of integers/floats")
     squared_matrix = []
     for i in matrix:
         squared_matrix.append([round(j / div, 2) for j in i])
