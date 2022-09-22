f562e8f81cdfe12bd58337b4c7e45a4a92538f0f
mateo <mateo@localhost.localdomain>
2022-09-22 12:39:28 -0300

    Task 0 fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,6 +6,10 @@ def add_integer(a, b=98):
     """Adds two integers"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
+    if a is None:
+        raise TypeError("a must be an integer")
+    if b is None:
+        raise TypeError("b must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
