46e7fa9b05256e31a272c52784ce7a097e01ae39
mateo <mateo@localhost.localdomain>
2022-09-22 16:15:54 -0300

    Task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@
 def matrix_divided(matrix, div):
     """Divides a matrix"""
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists)" +
+        raise TypeError("matrix must be a matrix (list of lists) " +
                         "of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -17,11 +17,11 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
     for j in matrix:
         if not isinstance(j, list):
-            raise TypeError("matrix must be a matrix (list of lists)" +
+            raise TypeError("matrix must be a matrix (list of lists) " +
                             "of integers/floats")
         for k in j:
             if type(k) is not int and type(k) is not float:
-                raise TypeError("matrix must be a matrix (list of lists)" +
+                raise TypeError("matrix must be a matrix (list of lists) " +
                                 "of integers/floats")
     squared_matrix = []
     for i in matrix:
