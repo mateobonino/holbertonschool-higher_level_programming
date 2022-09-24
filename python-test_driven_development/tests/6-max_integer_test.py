fa8d664f747bf8e79ba7ceec15800b1350b14354
Mateo Bonino <mateo@localhost.localdomain>
2022-09-23 22:40:07 -0300

    Task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Unit testing"""
-
-
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
-
 
 class TestMaxInteger(unittest.TestCase):
     def normal_list(self):
