07d3ad13b7695a652c3fcca7f326a7d92c0e6478
Mateo Bonino <mateo@localhost.localdomain>
2022-09-25 23:21:04 -0300

    Task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,14 +5,18 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+
     def normal_list(self):
         self.assertEqual(max_integer([1, 3, 7, 11]), 11)
 
-    def negative_expected(self):
-        self.assertEqual(max_integer([-5, -15, -3, -6]), -15)
+    def test_2(self):
+        self.assertEqual(max_integer([1, 11, 7]), 11)
+
+    def test_3(self):
+        self.assertEqual(max_integer([11, 1, 7]), 11)
 
     def empty_list(self):
-        self.assertEqual(max_integer([]), None)
+        self.assertRaises(TypeError, max_integer([]), None)
 
 
 if __name__ == "__main__":
