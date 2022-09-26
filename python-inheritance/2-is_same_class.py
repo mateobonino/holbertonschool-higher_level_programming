9abc0bd333b415864b86a1da4c775fc43175ce29
Mateo Bonino <mateo@localhost.localdomain>
2022-09-26 20:25:40 -0300

    Task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Is same class function"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if the object is an instance of the class"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
