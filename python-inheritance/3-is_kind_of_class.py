fdac68ccddefb222017e2df1934fa04feb69e198
Mateo Bonino <mateo@localhost.localdomain>
2022-09-26 20:35:40 -0300

    Task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Is kind of class Function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns TRUE if the object is kind of an instance of the class
+    otherwise, returns false
+    """
+    return isinstance(obj, a_class)
