910931a2feeeb5238d54363bfc2e4cf281b59b41
Mateo Bonino <mateo@localhost.localdomain>
2022-09-27 16:16:28 -0300

    Task 6 Fixed Documentation

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,6 +6,6 @@ import json
 
 def load_from_json_file(filename):
     """creates an Object from a “JSON file"""
-    with open(filename, "a") as f:
-        return json.load(f)
+    with open(filename, 'r') as f:
+        return json.loads(f)
     f.close()
