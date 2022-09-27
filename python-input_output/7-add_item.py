fca2de82132a7c0afe6e0a38da5f5eaffd836682
Mateo Bonino <mateo@localhost.localdomain>
2022-09-27 16:37:46 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,9 +10,15 @@ import json
 
 items_list = []
 
-with open('add_item.json', 'w+') as f:
-    items_list = load_from_json_file('add_item.json')
-
-for i in sys.argv[1]:
-    items_list.append(i)
-save_to_json_file(items_list, 'add_item.json')
+try:
+    with open('add_item.json', 'w') as f:
+        items_list = load_from_json_file('add_item.json')
+        for i in sys.argv[1:]:
+            items_list.append(i)
+        f.close()
+except IOError:
+    with open('add_item.json', 'w+') as f:
+        items_list = load_from_json_file('add_item.json')
+        for i in sys.argv[1:]:
+            items_list.append(i)
+        f.close()
