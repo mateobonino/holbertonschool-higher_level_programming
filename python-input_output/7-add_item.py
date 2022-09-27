f715e8e4cb836542ca36b5156ddffdc6ae730674
Mateo Bonino <mateo@localhost.localdomain>
2022-09-27 16:30:44 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list"""
+"""adds all arguments to a Python list, and then save them to a file"""
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -11,7 +11,7 @@ import json
 items_list = []
 
 with open('add_item.json', 'w+') as f:
-    items_list = load_from_json_file(f)
+    items_list = load_from_json_file('add_item.json')
 
 for i in sys.argv[1]:
     items_list.append(i)
