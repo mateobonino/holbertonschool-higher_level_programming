38c3f608f72507a2f6b03d7d61472e0f7770fb44
Mateo Bonino <mateo@localhost.localdomain>
2022-09-27 00:58:13 -0300

    Task 10 FIx

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,6 +7,7 @@ Rectangle = __import__('9-rectangle.py').Rectangle
 
 class Square(Rectangle):
     """Square Class"""
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
