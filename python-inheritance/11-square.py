f90081b4b791d4d39aa339d6a2645a0957d460fb
Mateo Bonino <mateo@localhost.localdomain>
2022-09-27 15:44:50 -0300

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(size, size)
