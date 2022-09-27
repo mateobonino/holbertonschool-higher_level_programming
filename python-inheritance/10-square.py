841341ec79e371faecc7b21e474985d87ff84d44
Mateo Bonino <mateo@localhost.localdomain>
2022-09-27 00:53:51 -0300

    Task 10 FIx

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Base Geometry Class"""
+"""Square class that inherits from Rectangle and Base Geomatry Class"""
 
 
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a square that inherits from Rectangle class"""
+    """Defines a square that inherits from 'integer_validator' function
+    from Rectangle and Base Geometry Class
+    """
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
