67e11e21869a7fbfcaa64d878a9997c8385ee0c6
Mateo Bonino <mateo@localhost.localdomain>
2022-09-28 16:02:22 -0300

    Task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,7 +10,7 @@ class Student():
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs == None:
+        if attrs is None:
             return self.__dict__
 
         if isinstance(attrs, list):
