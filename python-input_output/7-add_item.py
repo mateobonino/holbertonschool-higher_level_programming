20889057ce4681048bbb1acbb98d376e7943296e
Mateo Bonino <mateo@localhost.localdomain>
2022-09-28 02:27:12 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,10 +11,11 @@ load_from_json_file == __import__('6-load_from_json_file').load_from_json_file
 
 
 def main():
+    items_list = []
     try:
         items_list = load_from_json_file('add_item.json')
     except:
-        items_list = []
+        pass
     for i in range(1, len(sys.argv)):
         items_list.append(sys.argv[1])
     save_to_json_file(items_list, 'add_item.json')
