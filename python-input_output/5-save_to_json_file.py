4f9e0b28ff9c6440905d639a20d5757d69fd74db
Mateo Bonino <mateo@localhost.localdomain>
2022-09-28 02:40:11 -0300

    Task 7

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Converts to String"""
-    with open(filename, "a") as f:
+    with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
     f.close()
