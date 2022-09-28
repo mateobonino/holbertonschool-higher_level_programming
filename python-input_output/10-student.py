3700762e7691b185c852a64abac4921e17c5842a
Mateo Bonino <mateo@localhost.localdomain>
2022-09-28 16:01:12 -0300

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student():
+    """Defines a Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs:
+            return self.__dict__
+
+        if isinstance(attrs, list):
+            attributes = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    attributes[i] = getattr(self, i)
+            return attributes
