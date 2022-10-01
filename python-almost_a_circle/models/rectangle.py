d858a0799b7565cc24bcb273c6e19cd6796fb29e
Mateo Bonino <mateo@localhost.localdomain>
2022-10-01 15:02:21 -0300

    Task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """Class that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super()__init__(id)
