078faf5aa9f02e18f6baf37d454b6733a532d012
Mateo Bonino <mateo@localhost.localdomain>
2022-10-03 15:36:35 -0300

    Task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,7 +92,7 @@ class Rectangle(Base):
             print()
         for i in range(self.__height):
             for x in range(self.__x):
-                print("", end="")
+                print(" ", end="")
             for k in range(self.__width):
                 print("#", end="")
             print()
