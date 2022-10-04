e1bf7595227eb8a7e48b6b5c0e56d92538f4f951
Mateo Bonino <mateo@localhost.localdomain>
2022-10-04 20:33:58 -0300

    Task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,7 +22,7 @@ class Square(Rectangle):
         return self.__width
 
     @size.setter
-    def width(self, value):
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
