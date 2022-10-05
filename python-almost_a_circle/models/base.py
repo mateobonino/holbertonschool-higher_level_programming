9294d0f036d93076eec3ac527070b201d90968d0
Mateo Bonino <mateo@localhost.localdomain>
2022-10-05 03:56:28 -0300

    Task 12

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base():
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
