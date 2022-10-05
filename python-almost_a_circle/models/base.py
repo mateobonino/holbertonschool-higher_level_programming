2da69df87f1a3ae2348c5fed8d01e911ce4df2d4
Mateo Bonino <mateo@localhost.localdomain>
2022-10-05 03:55:55 -0300

    Task 12

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base():
             self.id = id
 
     def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
             return '[]'
         return json.dumps(list_dictionaries)
