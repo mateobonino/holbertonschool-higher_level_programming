8a4962be0bd9ee151a1f9c6a50d3114c81ab3dc3
mateo <mateo@localhost.localdomain>
2022-10-06 15:49:58 -0300

    Task 0

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -22,3 +22,40 @@ class Test_Rectangle(unittest.TestCase):
         self.assertEqual(rect3.height, 2)
         self.assertEqual(rect3.x, 3)
         self.assertEqual(rect3.y, 4)
+
+    def test_6(self):
+        rect4 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(rect4.id, 5)
+        self.assertEqual(rect4.width, 1)
+        self.assertEqual(rect4.height, 2)
+        self.assertEqual(rect4.x, 3)
+        self.assertEqual(rect4.y, 4)
+
+    def test_7(self):
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, -4)
+        
+    def test_8(self):
+        rect5 = Rectangle(3, 3)
+        self.assertEqual(rect5.id, 10)
+        self.assertEqual(rect5.width, 3)
+        self.assertEqual(rect5.height, 3)
+        self.assertEqual(rect5.area(), 9)
