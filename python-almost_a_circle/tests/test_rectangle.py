a489bd38f2f9b81f59d77d2f7fd7898745b0721b
mateo <mateo@localhost.localdomain>
2022-10-06 15:35:47 -0300

    Task 0

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -4,21 +4,21 @@ from models.rectangle import Rectangle
 class Test_Rectangle(unittest.TestCase):
     def test_1(self):
         rect = Rectangle(1, 2)
-        self.assertEqual(rect.id, 1)
+        self.assertEqual(rect.id, 7)
         self.assertEqual(rect.width, 1)
         self.assertEqual(rect.height, 2)
 
     def test_2(self):
-        rect = Rectangle(1, 2, 3)
-        self.assertEqual(rect.id, 2)
-        self.assertEqual(rect.width, 1)
-        self.assertEqual(rect.height, 2)
-        self.assertEqual(rect.x, 3)
+        rect2 = Rectangle(1, 2, 3)
+        self.assertEqual(rect2.id, 8)
+        self.assertEqual(rect2.width, 1)
+        self.assertEqual(rect2.height, 2)
+        self.assertEqual(rect2.x, 3)
 
     def test_3(self):
-        rect = Rectangle(1, 2, 3, 4)
-        self.assertEqual(rect.id, 3)
-        self.assertEqual(rect.width, 1)
-        self.assertEqual(rect.height, 2)
-        self.assertEqual(rect.x, 3)
-        self.assertEqual(rect.y, 4)
+        rect3 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rect3.id, 9)
+        self.assertEqual(rect3.width, 1)
+        self.assertEqual(rect3.height, 2)
+        self.assertEqual(rect3.x, 3)
+        self.assertEqual(rect3.y, 4)
