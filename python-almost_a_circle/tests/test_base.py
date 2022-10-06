22a38431a747e5465d60ef4abadbea8e7c8e18aa
mateo <mateo@localhost.localdomain>
2022-10-06 15:06:28 -0300

    Task 0 Fix

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -6,8 +6,11 @@ class Test_Base(unittest.TestCase):
     def test_1(self):
         self.assertEqual(Base().id, 1)
 
+    def test_2(self):
         self.assertEqual(Base().id, 2)
 
+    def test_3(self):
         self.assertEqual(Base(59).id, 59)
 
+    def test_4(self):
         self.assertEqual(Base().to_json_string([]), '[]')
