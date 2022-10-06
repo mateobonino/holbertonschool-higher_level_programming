65159bea19252e70e1825fe09f973121effd5b03
mateo <mateo@localhost.localdomain>
2022-10-06 14:38:49 -0300

    Task 0

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -7,14 +7,10 @@ from models.base import Base
 
 class Test_Base(unittest.TestCase):
     def test_1(self):
-        base1 = Base()
-        self.assertEqual(base1.id, 1)
+        self.assertEqual(Base().id, 1)
 
-        base2 = Base()
-        self.assertEqual(base2.id, 2)
+        self.assertEqual(Base().id, 2)
 
-        base3 = Base(89)
-        self.assertEqual(base3.id, 89)
+        self.assertEqual(Base(59).id, 59)
 
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(Base().to_json_string([]), [])
