7e5efb0943ecd81c22f6b71bfcb45e8c15959459
Mateo Bonino <mateo@localhost.localdomain>
2022-10-06 23:21:56 -0300

    Task 0

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -10,14 +10,14 @@ class Test_Rectangle(unittest.TestCase):
 
     def test_2(self):
         rect2 = Rectangle(1, 2, 3)
-        self.assertEqual(rect2.id, 10)
+        self.assertEqual(rect2.id, 16)
         self.assertEqual(rect2.width, 1)
         self.assertEqual(rect2.height, 2)
         self.assertEqual(rect2.x, 3)
 
     def test_3(self):
         rect3 = Rectangle(1, 2, 3, 4)
-        self.assertEqual(rect3.id, 11)
+        self.assertEqual(rect3.id, 17)
         self.assertEqual(rect3.width, 1)
         self.assertEqual(rect3.height, 2)
         self.assertEqual(rect3.x, 3)
@@ -55,18 +55,18 @@ class Test_Rectangle(unittest.TestCase):
         
     def test_8(self):
         rect5 = Rectangle(3, 3)
-        self.assertEqual(rect5.id, 12)
+        self.assertEqual(rect5.id, 18)
         self.assertEqual(rect5.width, 3)
         self.assertEqual(rect5.height, 3)
         self.assertEqual(rect5.area(), 9)
 
     def test_9(self):
         rect6 = Rectangle(3, 3, 3, 3)
-        self.assertEqual(rect6.id, 13)
+        self.assertEqual(rect6.id, 19)
         self.assertEqual(rect6.width, 3)
         self.assertEqual(rect6.height, 3)
         self.assertEqual(rect6.area(), 9)
-        self.assertEqual(rect6.__str__(), "[Rectangle] (13) 3/3 - 3/3")
+        self.assertEqual(rect6.__str__(), "[Rectangle] (19) 3/3 - 3/3")
 
     def test_10(self):
         rect7 = Rectangle(3, 2, 0, 0)
@@ -90,3 +90,11 @@ class Test_Rectangle(unittest.TestCase):
         recc1 = Rectangle(3, 3, 2, 0, 56)
         recc2 = Rectangle(4, 4, 5, 3, 15)
         self.assertEqual(Rectangle.save_to_file([recc1, recc2]), 0)
+
+    def test_15(self):
+        Rectangle.load_from_file()
+        recc1 = Rectangle(3, 3, 4, 0)
+        recc2 = Rectangle(5, 5)
+        list_rectangles_input = [recc1, recc2]
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
