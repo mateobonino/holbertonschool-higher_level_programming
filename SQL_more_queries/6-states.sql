f9a8f8e3c7a75bcae2a867b6ea7e16c1441e6122
Mateo <mbonino1810@gmail.com>
2022-11-03 15:35:20 -0700

    Task 6 Fix

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- task 6
 -- create database
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL
 );
