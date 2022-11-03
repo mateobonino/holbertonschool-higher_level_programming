8dff90c191541c829f9965280f30be105ebf7f82
Mateo Bonino <mbonino1810@gmail.com>
2022-11-03 20:55:40 -0300

    Task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- task 9
 -- cities by state
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states AS st
-ON cities.state_id = st.id;
+INNER JOIN states
+ON cities.state_id = states.id;
+ORDER BY cities.id;
