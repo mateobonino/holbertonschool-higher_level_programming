98cb26934ac46d6fcccc81e6602518e3510ed6cf
Mateo <mbonino1810@gmail.com>
2022-11-03 13:07:29 -0700

    Task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
 -- task 16
 -- display records where name is not null
-SELECT score, name FROM second_database WHERE name != NULL ORDER BY score DESC;
+SELECT score, name FROM second_table 
+WHERE name != NULL 
+ORDER BY score DESC;
