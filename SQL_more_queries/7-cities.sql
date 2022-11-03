e7bb7554ebe96d3ee80396752cbc6e8d64e5483e
Mateo Bonino <mbonino1810@gmail.com>
2022-11-03 20:15:27 -0300

    Task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- task 7
+-- create databse hbtn_0d_usa and cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+
+)
