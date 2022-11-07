5a8e631e0a1016e890add793821383138b99b982
Mateo <mbonino1810@gmail.com>
2022-11-07 09:57:20 -0300

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Script safe from SQL injections
+
+'''
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                         host="localhost", port=3306)
+    cursor = db.cursor()
+    arg4 = sys.argv[4]
+    cursor.execute(f"SELECT * FROM states WHERE BINARY name = '%s'" % (arg4,))
+    for i in cursor.fetchall():
+        print(i)
