bb1be18a6bbb8f84def94f918d17fb2629af6c59
Mateo <mbonino1810@gmail.com>
2022-11-07 03:57:20 -0800

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-# lists the content of "states" table from "hbtn_0e_0_usa" database
-# pass args via the command line to specify the user, password and database
-
+'''
+Lists the content of "states" table from "hbtn_0e_0_usa" database,
+pass args via the command line to specify the user, password and database
+'''
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                     host="localhost", port=3306)
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                         host="localhost", port=3306)
 
-cursor = db.cursor()
-cursor.execute("SELECT * from states")
-states = cursor.fetchall()
-for i in states:
-    print(i)
+    cursor = db.cursor()
+    cursor.execute("SELECT * from states")
+    states = cursor.fetchall()
+    for i in states:
+        print(i)
