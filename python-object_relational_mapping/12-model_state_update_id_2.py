4a9d0cd1c7a91a68498c6941f2904fbf2613c248
Mateo <mbonino1810@gmail.com>
2022-11-07 21:34:50 -0300

    Task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+'''
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import State, Base
+from sqlalchemy import update
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    host = "localhost"
+    engine = sqlalchemy.create_engine("mysql+mysqldb://{}:{}@{}/{}"
+                                      .format(user, passwd, host, database),
+                                      pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for i in session.query(State).order_by(State.id).all():
+        if i.id == 2:
+            i.name = "New Mexico"
