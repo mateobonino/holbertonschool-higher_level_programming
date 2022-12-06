ecbfc73acbd9ed8ac612b7716c81624a0f82e801
Mateo Bonino <mateo@localhost.localdomain>
2022-12-06 03:25:42 -0300

    Task 3 Fixed

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv.length <= 2) {
+let counter = 0;
+const args = process.argv;
+args.forEach((value, index) => {
+  counter++;
+});
+if (counter <= 2) {
   console.log('No argument');
 } else {
-  console.log(process.argv[2]);
+  for (let i = 2; i < counter; i++) {
+    console.log(process.argv[i]);
+  }
 }
