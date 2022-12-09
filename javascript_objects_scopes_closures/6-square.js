cdb2a6723b56e9a119d913b4b5295747f39da0d5
Mateo Bonino <mateo@localhost.localdomain>
2022-12-09 18:00:14 -0300

    Task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const MySquare = require('./5-square');
 
 class Square extends MySquare {
   charPrint (c) {
-    if (c === undefined) {
+    if (!c) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
