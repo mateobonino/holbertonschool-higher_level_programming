62a07a9257ffb22a2b0b7d8ce4b0e6b29ccd57f3
Mateo Bonino <mateo@localhost.localdomain>
2022-12-18 02:13:49 -0300

    Task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,5 +6,14 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  
+  const data = JSON.parse(body);
+  let count = 0;
+  for (const i of data.results) {
+    for (const j of i.characters) {
+      if (j.includes('people/18/')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
