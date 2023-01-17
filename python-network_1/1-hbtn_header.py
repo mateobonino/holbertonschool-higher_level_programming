ea21bffb3728f0d2424534825ae08e27628a76b5
Mateo Bonino <mateo@localhost.localdomain>
2023-01-17 17:57:33 -0300

    documentation xd

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,5 +3,5 @@ from urllib import request
 from sys import argv
 
 with request.urlopen(argv[1]) as response:
-    body = response.info()
-print(body["X-Request-Id"])
+    body = response.info()["X-Request-Id"]
+print(body)
