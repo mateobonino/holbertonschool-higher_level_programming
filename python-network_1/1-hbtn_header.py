a56b546581831ff1dc1c93be0729c4c3a70a5d27
Mateo Bonino <mbonino1810@gmail.com>
2023-01-17 14:47:45 -0300

    task 1 fixed documentation

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# Takes the response header from the URL
+"""
+Takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in
+the header of the response
+"""
 import urllib.request
 import sys.argv
 
