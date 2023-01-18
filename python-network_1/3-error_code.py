18ee1ca0425c6d09a068131e7775a3eff9834e00
Mateo Bonino <mbonino1810@gmail.com>
2023-01-18 02:56:49 -0300

    task 3

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Takes in a URL, sends a request to the URL
+    and displays the body of the response
+"""
+if __name__ == "__main__":
+    from urllib import error, request
+    from sys import argv
+    try:
+        with request.urlopen(argv[1]) as response:
+            myResponse = response.read().decode('utf-8')
+    except Exception as error:
+        print("Error code: {}".format(error.__dict__['code']))
