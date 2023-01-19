d0ec1cd6ed51cf3c00557f35626e34c805ada8f4
Mateo Bonino <mbonino1810@gmail.com>
2023-01-19 17:17:43 -0300

    task 8

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Takes in a letter and sends a POST
+    request to http://0.0.0.0:5000/search_user
+    with the letter as parameter
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    if argv[1]:
+        myData = {"q": argv[1]}
+    else:
+        myData = {"q": ""}
+    url = "http://0.0.0.0:5000/search_user"
+    with requests.post(url, data=myData) as response:
+        try:
+            myResponse = response.json()
+        except Exception as invalid:
+            print("Not a valid JSON")
+        if 'id' not in myResponse.keys() or 'name' not in myResponse.keys():
+            print("[{}] {}".format(myResponse['id'], myResponse['name']))
+        else:
+            print("No result")
